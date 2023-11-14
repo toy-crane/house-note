1ad55ede457de045b4ab3060bce5974ceb6dbbd3
toycrane <alwaysfun2183@gmail.com>
2023-11-14 16:26:08 +0900

    refactor: Remove unused imports and transpile package

--- apps/nextjs/src/utils/api.ts
+++ apps/nextjs/src/utils/api.ts
@@ -1,6 +1,3 @@
-import type { AppRouter } from "@house-note/api";
 import { createTRPCReact } from "@trpc/react-query";
 
-export const api = createTRPCReact<AppRouter>();
-
-export { type RouterInputs, type RouterOutputs } from "@house-note/api";
+export const api = createTRPCReact();
