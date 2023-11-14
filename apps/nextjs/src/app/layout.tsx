34dc299b305753a12792de9851710681f29ee2f5
toycrane <alwaysfun2183@gmail.com>
2023-11-14 16:48:13 +0900

    refactor(layout): import trpc provider from different path

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -3,9 +3,9 @@ import { Inter } from "next/font/google";
 
 import "~/styles/globals.css";
 
-import { headers } from "next/headers";
+import { cookies } from "next/headers";
 
-import { TRPCReactProvider } from "./providers";
+import { TRPCReactProvider } from "~/trpc/react";
 
 const fontSans = Inter({
   subsets: ["latin"],
@@ -39,7 +39,7 @@ export default function Layout(props: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={["font-sans", fontSans.variable].join(" ")}>
-        <TRPCReactProvider headers={headers()}>
+        <TRPCReactProvider cookies={cookies().toString()}>
           {props.children}
         </TRPCReactProvider>
       </body>
