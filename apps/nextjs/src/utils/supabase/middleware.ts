c1314902a682ed6bbd769989ac9360cf5d7207b5
toycrane <alwaysfun2183@gmail.com>
2023-11-15 14:29:45 +0900

    fix(supabase): type이 안맞는 cookie Option 수정

--- apps/nextjs/src/utils/supabase/middleware.ts
+++ apps/nextjs/src/utils/supabase/middleware.ts
@@ -2,23 +2,51 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { createServerClient } from "@supabase/ssr";
 
-import { env } from "~/env.mjs";
-
 export const createClient = (request: NextRequest) => {
   // Create an unmodified response
-  const response = NextResponse.next({
+  let response = NextResponse.next({
     request: {
       headers: request.headers,
     },
   });
 
+  // FIX: type이 안맞아서 cookie Option 제거함
   const supabase = createServerClient(
-    env.NEXT_PUBLIC_SUPABASE_URL,
-    env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
         get(name: string) {
           return request.cookies.get(name)?.value;
+        },
+        set(name: string, value: string) {
+          // If the cookie is updated, update the cookies for the request and response
+          request.cookies.set({
+            name,
+            value,
+          });
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          });
+          response.cookies.set({
+            name,
+            value,
+          });
+        },
+        remove(name: string) {
+          // If the cookie is removed, update the cookies for the request and response
+          request.cookies.set(name, "");
+          response = NextResponse.next({
+            request: {
+              headers: request.headers,
+            },
+          });
+          response.cookies.set({
+            name,
+            value: "",
+          });
         },
       },
     },
--- apps/nextjs/src/utils/supabase/server.ts
+++ apps/nextjs/src/utils/supabase/server.ts
@@ -1,4 +1,5 @@
 import type { cookies } from "next/headers";
+import type { CookieOptions } from "@supabase/ssr";
 import { createServerClient } from "@supabase/ssr";
 
 import { env } from "~/env.mjs";
@@ -11,6 +12,26 @@ export const createClient = (cookieStore: ReturnType<typeof cookies>) => {
       cookies: {
         get(name: string) {
           return cookieStore.get(name)?.value;
+        },
+        set(name: string, value: string, options: CookieOptions) {
+          try {
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+            cookieStore.set(name, value, options);
+          } catch (error) {
+            // The `set` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+        remove(name: string, options: CookieOptions) {
+          try {
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+            cookieStore.set(name, "", options);
+          } catch (error) {
+            // The `delete` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
         },
       },
     },
