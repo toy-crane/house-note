3cbe6131cd74c6366e03d6b8c2a20fb331fef5ed
toycrane <alwaysfun2183@gmail.com>
2023-11-17 21:46:34 +0900

    refactor(page): remove unnecessary code

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,19 +1,13 @@
 import { api } from "~/trpc/server";
-import { CreatePost } from "./_components/create-post";
 import AuthButton from "./_components/auth-button";
+import { CreatePost } from "./_components/create-post";
 
-export default async function HomePage() {
-  const posts = await api.post.postList.query();
+export default function HomePage() {
   return (
     <main className="flex h-screen flex-col">
       <AuthButton />
       <div className="container mt-12 flex flex-col items-center justify-center gap-4 py-8">
         initialize Page
-        <div>
-          {posts.map((p) => (
-            <div key={p.id}>{p.name}</div>
-          ))}
-        </div>
         <button
           onClick={async () => {
             "use server";
