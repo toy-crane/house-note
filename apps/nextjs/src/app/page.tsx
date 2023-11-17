606824c236cd2fbed1a58836b9907005a51e2557
toycrane <alwaysfun2183@gmail.com>
2023-11-17 21:49:10 +0900

    refactor(page): Remove unused code and button to create post

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,4 +1,3 @@
-import { api } from "~/trpc/server";
 import AuthButton from "./_components/auth-button";
 import { CreatePost } from "./_components/create-post";
 
@@ -8,14 +7,6 @@ export default function HomePage() {
       <AuthButton />
       <div className="container mt-12 flex flex-col items-center justify-center gap-4 py-8">
         initialize Page
-        <button
-          onClick={async () => {
-            "use server";
-            await api.post.create.mutate({ name: "hello" });
-          }}
-        >
-          create Post
-        </button>
         <CreatePost />
       </div>
     </main>
